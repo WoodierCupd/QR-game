6694a2ea11c30d1ae6b1ec44288a54760a26be83
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-13 23:10:20 +0200

    Log in link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,7 @@
-<div class="flex justify-center h-16">
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+<div class="flex h-16">
+    @auth
+        <a href="{{ url('/dashboard') }}">Klik <span class="text-blue-400">hier</span> om naar het dashboard te gaan.</a>
+    @else
+        <a href="{{ route('login') }}">Bent u een docent? Log dan <span class="text-blue-400">hier</span> in.</a>
+    @endauth
 </div>
