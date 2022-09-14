d2fa67ffa5bd7f76d8482d226e856afd36eb461b
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-14 16:49:05 +0200

    Form for csv file

--- app/Http/Livewire/CsvUpload.php
+++ app/Http/Livewire/CsvUpload.php
@@ -3,9 +3,23 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CsvUpload extends Component
 {
+    use WithFileUploads;
+
+    public $file;
+
+    public function save()
+    {
+        $this->validate([
+            'file' => 'required|max:1024|mimes:csv,txt', // 1MB Max
+        ]);
+
+        $this->file->store('file');
+    }
+
     public function render()
     {
         return view('livewire.csv-upload');
--- resources/views/livewire/csv-upload.blade.php
+++ resources/views/livewire/csv-upload.blade.php
@@ -1,6 +1,8 @@
 <div>
-    <div class="flex flex-col">
-        <label class="" for="csv">Upload hier een <span class="font-bold">CSV</span> bestand met vragen.</label>
-        <input class="form-input m-0 p-2 rounded text-black" name="csv" type="file">
-    </div>
+    <form wire:submit.prevent="save" class="flex flex-col">
+        <label for="csv">Upload hier een <span class="font-bold">CSV</span> bestand met vragen.</label>
+        <input wire:model="file" class="form-input m-0 p-1 rounded text-black h-10" name="csv" type="file">
+        @error('file') <span class="error">{{ $message }}</span> @enderror
+        <button class="btn h-10 font-bold bg-black rounded mt-2.5" type="submit">Generate QR-Codes</button>
+    </form>
 </div>
