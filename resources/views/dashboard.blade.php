4c4cf9e3a3c86eea6d4a1719aee6674a184aecbe
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-14 12:11:01 +0200

    Made livewire work

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,13 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-    <div class="container mx-auto">
-        <div class="mt-5 flex flex-col items-center">
-            <livewire:csv-upload/>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <x-head></x-head>
+    <body class="container mx-auto bg-gradient-to-r from-dark_red to-almost_black text-white h-screen flex flex-col">
+        <x-header></x-header>
+        <div class="container mx-auto">
+            <div class="mt-5 flex flex-col items-center">
+                <livewire:csv-upload/>
+            </div>
         </div>
-    </div>
-</x-app-layout>
+        @livewireScripts
+    </body>
+</html>
--- resources/views/livewire/csv-upload.blade.php
+++ resources/views/livewire/csv-upload.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex flex-col">
         <label class="" for="csv">Upload hier een <span class="font-bold">CSV</span> bestand met vragen.</label>
-        <input class="form-input m-0 p-2 rounded" name="csv" type="file">
+        <input class="form-input m-0 p-2 rounded text-black" name="csv" type="file">
     </div>
 </div>
