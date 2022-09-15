14ca54e5dbece984c6adca3049935644b4e5b866
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-15 14:38:19 +0200

    Back button on dashboard

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,6 @@
-<div class="flex justify-center h-16">
+<div class="flex justify-center md:justify-start h-16">
+    @if(\Request::route()->getName() == 'dashboard')
+        <button class="px-5"><a href="{{route('welcome')}}">Back</a></button>
+    @endif
     {{--TODO: Make open camera button for mobile to scan the QR-codes--}}
 </div>
