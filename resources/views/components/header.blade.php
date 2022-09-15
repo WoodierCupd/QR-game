36fb4b8a88f6a7e7690a259cba977f293e478804
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-15 15:30:23 +0200

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-center md:justify-start h-16">
-    @if(\Request::route()->getName() == 'dashboard')
+    @if(\Request::route()->getName() != 'welcome')
         <button class="px-5"><a href="{{route('welcome')}}">Back</a></button>
     @endif
     {{--TODO: Make open camera button for mobile to scan the QR-codes--}}
