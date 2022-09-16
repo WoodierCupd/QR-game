0f6d322709df99c53d628b00dc5c7b4e86de24dd
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-16 12:38:48 +0200

    Create 2022_09_16_102846_create_score_table.php

---
+++ database/migrations/2022_09_16_102846_create_score_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('score', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_number');
+            $table->unsignedBigInteger('question_id');
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->boolean('correct');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('score');
+    }
+};
