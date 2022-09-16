66fbd3beaaf3a3b7f71f32cec676753a1a2a906a
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-16 15:04:34 +0200

    Update question-form.blade.php

--- resources/views/livewire/question-form.blade.php
+++ resources/views/livewire/question-form.blade.php
@@ -1,20 +1,26 @@
-<div class="flex flex-col w-full items-center ">
-    @if($done !== true)
-        <div class="text-3xl">
-            <p>{{$question->question}}</p>
-        </div>
-        <div class="flex flex-col mt-5">
-            <button wire:click="answer_a" class="bg-white text-black rounded p-1.5"><p class="text-xl text-left">A. {{$question->option_a}}</p></button>
-            <button wire:click="answer_b" class="bg-white text-black rounded p-1.5 mt-2.5"><p class="text-xl text-left">B. {{$question->option_b}}</p></button>
-            <button wire:click="answer_c" class="bg-white text-black rounded p-1.5 mt-2.5"><p class="text-xl">C. {{$question->option_c}}</p></button>
-        </div>
-    @else
-        <div class="text-3xl">
-            @if(isset($score->correct))
-                <p>@if($score->correct == true) Je hebt de vraag correct beantwoord. @else Je hebt de vraag fout beantwoord. @endif</p>
-            @else
-                <p>Je hebt deze vraag al beantwoord.</p>
-            @endif
-        </div>
-    @endif
-</div>
+@if (Cookie::get('number') === null)
+    <div class="flex flex-col w-full items-center ">
+        <a href="{{route('welcome')}}"><p class="text-5xl font-bold underline">Please enter your student number here!</p></a>
+    </div>
+@else
+    <div class="flex flex-col w-full items-center ">
+        @if($done !== true)
+            <div class="text-3xl">
+                <p>{{$question->question}}</p>
+            </div>
+            <div class="flex flex-col mt-5">
+                <button wire:click="answer_a" class="bg-white text-black rounded p-1.5"><p class="text-xl text-left">A. {{$question->option_a}}</p></button>
+                <button wire:click="answer_b" class="bg-white text-black rounded p-1.5 mt-2.5"><p class="text-xl text-left">B. {{$question->option_b}}</p></button>
+                <button wire:click="answer_c" class="bg-white text-black rounded p-1.5 mt-2.5"><p class="text-xl">C. {{$question->option_c}}</p></button>
+            </div>
+        @else
+            <div class="text-3xl">
+                @if(isset($score->correct))
+                    <p>@if($score->correct == true) Je hebt de vraag correct beantwoord. @else Je hebt de vraag fout beantwoord. @endif</p>
+                @else
+                    <p>Je hebt deze vraag al beantwoord.</p>
+                @endif
+            </div>
+        @endif
+    </div>
+@endif
