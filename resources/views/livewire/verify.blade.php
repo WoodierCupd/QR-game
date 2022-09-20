dfb3d9518cd64fb147da37456971a1f477a88710
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-20 16:28:41 +0200

    verify component

---
+++ resources/views/livewire/verify.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-1 flex-col justify-center items-center flex-1">
+    <h1 class="font-extrabold text-6xl mb-2.5 font-rammetto">Verify</h1>
+    <button class="btn mb-2 w-28 h-10 font-bold bg-green-500 rounded">Goed</button>
+    <button class="btn w-28 h-10 font-bold bg-red-500 rounded">Fout</button>
+{{--    @error('verify') <span class="error">{{ $message }}</span> @enderror--}}
+</div>
