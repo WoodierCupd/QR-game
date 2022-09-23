aaedd6901609826d013d47941f28014c1038e731
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-23 09:44:13 +0200

    Verify works now!

--- app/Http/Livewire/Verify.php
+++ app/Http/Livewire/Verify.php
@@ -2,13 +2,49 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Score;
+use App\Models\verify_request;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class Verify extends Component
 {
     public $verify_request;
+    public $value;
 
-public function render()
+    public function good(){
+        $this->value = true;
+        $this->teacher();
+    }
+
+    public function bad(){
+        $this->value = false;
+        $this->teacher();
+    }
+
+    private function teacher(){
+        if ($this->value){
+            $score = Score::create([
+                'student_number' => $this->verify_request->student_number,
+                'question_id' => $this->verify_request->question_id,
+                'correct' => 1,
+            ]);
+            verify_request::find($this->verify_request->id)->delete();
+            Storage::delete("public.{$this->verify_request->image_path}");
+            return redirect()->to(route('dashboard'));
+        } else {
+            $score = Score::create([
+                'student_number' => $this->verify_request->student_number,
+                'question_id' => $this->verify_request->question_id,
+                'correct' => 0,
+            ]);
+            verify_request::find($this->verify_request->id)->delete();
+            Storage::delete($this->verify_request->image_path);
+            return redirect()->to(route('dashboard'));
+        }
+    }
+
+    public function render()
     {
         return view('livewire.verify');
     }
