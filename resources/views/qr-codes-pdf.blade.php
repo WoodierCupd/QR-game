b51887cc6814eb799630a32a3ac3e18012c0e749
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-27 11:23:28 +0200

    Update qr-codes-pdf.blade.php

--- resources/views/qr-codes-pdf.blade.php
+++ resources/views/qr-codes-pdf.blade.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 <h1>{{ $title }}</h1>
-<div class="w-full flex flex-col justify-center">
+<div>
 @foreach($questions as $question)
     <div>
-        <h3 class="text-3xl text-center">{{$question['original_id']}}</h3>
-        <img class="block object-cover object-center w-full h-full" src="{{ public_path("{$question['qr_path']}") }}" style="width: 200px; height: 200px">
+        <h3>{{$question['original_id']}}</h3>
+        <img src="{{ public_path("{$question['qr_path']}") }}" style="width: 200px; height: 200px">
     </div>
 @endforeach
 </div>
