001a1475726aaeb3fc03714f6d755c4ba7ca3bb9
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-09-27 12:17:02 +0200

    count

--- app/Http/Livewire/StudentNumber.php
+++ app/Http/Livewire/StudentNumber.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Score;
+use App\Models\verify_request;
 use Illuminate\Support\Facades\Cookie;
 use Livewire\Component;
 
 class StudentNumber extends Component
 {
     public $number;
+    public $scores;
+    public $verify_requests;
 
     public function save(){
         $this->validate([
@@ -15,6 +19,17 @@ class StudentNumber extends Component
         ]);
         Cookie::queue('number', $this->number, 1440);
         return redirect(request()->header('Referer'));
+    }
+
+    public function mount(){
+        if (Cookie::get('number') !== null){
+            $this->scores = Score::where([
+                ['student_number', '=', $this->number],
+            ])->count();
+            $this->verify_requests = verify_request::where([
+                ['student_number', '=', $this->number],
+            ])->count();
+        }
     }
 
     public function render()
--- resources/views/livewire/student-number.blade.php
+++ resources/views/livewire/student-number.blade.php
@@ -2,6 +2,10 @@
     <h1 class="font-extrabold text-6xl mb-2.5 font-rammetto">QR-QUIZ</h1>
     @if (Cookie::get('number') !== null)
         <p>{{request()->cookie('number')}}</p>
+        <div>
+            <p>Je hebt {{$scores}} vragen beantwoord.</p>
+            <p>Er moeten nog {{$verify_requests}} vragen nagekeken worden.</p>
+        </div>
     @else
         <form wire:submit.prevent="save" class="flex flex-col bg-white text-center p-5 rounded">
             <input wire:model="number" class="input h-10 text-black text-center mb-2.5 rounded" type="text" placeholder="Leerling nummer">
